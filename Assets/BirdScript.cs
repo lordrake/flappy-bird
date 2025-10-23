9c252060e3192212527f17e2ee6e98a11f554db5
Danilo Colella <danilocolella17@gmail.com>
2025-10-23 10:38:15 +0200

    add game logic

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool isBirdAlive = true;
+
+    private float maxYOffset = 17;
+    private float minYOffset = -17;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isBirdAlive)
+        {
+            myRigidBody.linearVelocity = Vector2.up * flapStrength;
+        }
+
+        if (myRigidBody.position.y >  maxYOffset || myRigidBody.position.y < minYOffset)
+        {
+            logic.gameOver();
+            isBirdAlive = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        isBirdAlive = false;
+    }
+}
---
+++ Assets/LogicScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreIncrement)
+    {
+        playerScore += scoreIncrement;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);   
+    }
+}
